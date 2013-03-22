5daf7d397ccee906a97a0c2254e8d8b532cdbe73
lostleaf <lostleaf@d4a6f36f-6496-4f17-8535-afb2462302cb>
2013-03-22 05:23:02 +0000

    add all files
    git-svn-id: https://202.120.61.16/svn/lostleaf/compiler@3 d4a6f36f-6496-4f17-8535-afb2462302cb

---
+++ a.rb
@@ -0,0 +1,180 @@
+#!/usr/bin/env ruby
+
+code = <<CODE
+
+program: (declaration | functionDefinition)+
+       ;
+
+declaration: 'typedef' typeSpecifier declarators ';'
+           | typeSpecifier initDeclarators? ';'
+           ;
+
+functionDefinition: typeSpecifier plainDeclarator '(' parameters? ')' compoundStatement
+                   ;
+
+parameters: plainDeclaration (',' plainDeclaration)* (',' '...')?
+          ;
+
+declarators: declarator (',' declarator)*
+           ;
+
+initDeclarators: initDeclarator (',' initDeclarator)*
+                ;
+
+initDeclarator: declarator ('=' initializer)?
+               ;
+
+initializer: assignmentExpression
+           | '{' initializer+ '}'
+           ;
+
+typeSpecifier: 'void' | 'char' | 'int' | typedefName
+              | structOrUnion identifier? '{' plainDeclaration+ '}'
+              | structOrUnion identifier
+              ;
+
+structOrUnion: 'struct' | 'union'
+               ;
+
+plainDeclaration: typeSpecifier declarator+
+                 ;
+
+declarator: plainDeclarator '(' parameters? ')'
+          | plainDeclarator ('[' constantExpression ']')*
+          ;
+
+plainDeclarator: '*'* identifier
+                ;
+
+statement: expressionStatement
+         | compoundStatement
+         | selectionStatement
+         | iterationStatement
+         | jumpStatement
+         ;
+
+expressionStatement: expression? ';'
+                    ;
+
+compoundStatement: '{' declaration* statement* '}'
+                  ;
+
+selectionStatement: 'if' '(' expression ')' statement ('else' statement)?
+                   ;
+
+iterationStatement: 'while' '(' expression ')' statement
+                   | 'for' '(' expression? ';' expression? ';' expression? ')' statement
+                   ;
+
+jumpStatement: 'continue' ';'
+              | 'break' ';'
+              | 'return' expression? ';'
+              ;
+
+expression: assignmentExpression (',' assignmentExpression)*
+          ;
+
+assignmentExpression: logicalOrExpression
+                     | unaryExpression assignmentOperator assignmentExpression
+                     ;
+
+assignmentOperator: '=' | '*=' | '/=' | '%=' | '+=' | '-=' | '<<=' | '>>=' | '&=' | '^=' | '|='
+                   ;
+
+constantExpression: logicalOrExpression
+                   ;
+
+logicalOrExpression: logicalAndExpression ('||' logicalAndExpression)*
+                     ;
+
+logicalAndExpression: inclusiveOrExpression ('&&' inclusiveOrExpression)*
+                      ;
+
+inclusiveOrExpression: exclusiveOrExpression ('|' exclusiveOrExpression)*
+                       ;
+
+exclusiveOrExpression: andExpression ('^' andExpression)*
+                       ;
+
+andExpression: equalityExpression ('&' equalityExpression)*
+              ;
+
+equalityExpression: relationalExpression (equalityOperator relationalExpression)*
+                   ;
+
+equalityOperator: '==' | '!='
+                 ;
+
+relationalExpression: shiftExpression (relationalOperator shiftExpression)*
+                     ;
+
+relationalOperator: '<' | '>' | '<=' | '>='
+                   ;
+
+shiftExpression: additiveExpression (shiftOperator additiveExpression)*
+                ;
+
+shiftOperator: '<<' | '>>'
+              ;
+
+additiveExpression: multiplicativeExpression (additiveOperator multiplicativeExpression)*
+                   ;
+
+additiveOperator: '+' | '-'
+                 ;
+
+multiplicativeExpression: castExpression (multiplicativeOperator castExpression)*
+                         ;
+
+multiplicativeOperator: '*' | '/' | '%'
+                       ;
+
+castExpression: unaryExpression
+               | '(' typeName ')' castExpression
+               ;
+
+typeName: typeSpecifier '*'*
+         ;
+
+unaryExpression: postfixExpression
+                | '++' unaryExpression
+                | '--' unaryExpression
+                | unaryOperator castExpression
+                | 'sizeof' unaryExpression
+                | 'sizeof' '(' typeName ')'
+                ;
+
+unaryOperator: '&' | '*' | '+' | '-' | '~' | '!'
+              ;
+
+postfixExpression: primaryExpression postfix*
+                  ;
+
+postfix: '[' expression ']'
+       | '(' arguments? ')'
+       | '.' identifier
+       | '->' identifier
+       | '++'
+       | '--'
+       ;
+
+arguments: assignmentExpression (',' assignmentExpression)*
+         ;
+
+primaryExpression: identifier
+                  | constant
+                  | string
+                  | '(' expression ')'
+                  ;
+
+constant: integerConstant
+        | characterConstant
+        ;
+CODE
+
+new_code = code.gsub(/([A-Z])/) { |c|
+	"_#{$1.downcase}"
+}
+
+
+puts new_code
